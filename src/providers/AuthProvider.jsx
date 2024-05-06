ec9f9238f115d24839d4ccc531ea9959fd22dae2
sujonifty <sujonifty@gmail.com>
2024-05-06 23:34:46 +0600

    recape jwt

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -27,15 +27,22 @@ const AuthProvider = ({ children }) => {
 
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            const userEmail = currentUser?.email || user?.email;
+            const loggedUser ={email:userEmail};
             setUser(currentUser);
             console.log('current user', currentUser);
             setLoading(false);
             // if user exist then issue a token
             if(currentUser){
-                const loggedUser ={email:currentUser.email}
                 axios.post("http://localhost:5000/jwt",loggedUser,{withCredentials: true})
                 .then(res=>{
                     console.log('token response',res.data);
+                })
+            }
+            else{
+                axios.post("http://localhost:5000/logout",loggedUser,{withCredentials: true})
+                .then(res=>{
+                    console.log('token remove',res.data);
                 })
             }
         });
